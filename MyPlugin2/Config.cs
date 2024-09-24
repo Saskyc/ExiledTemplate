50b013513351afeacd168c828f93def2cbd61aeb
MSaska <martin.saska@student.sps-cl.cz>
2024-09-24 15:36:08 +0200

    Add project files.

---
+++ MyPlugin2/Config.cs
@@ -0,0 +1,12 @@
+﻿using Exiled.API.Interfaces;
+
+namespace MyPlugin2
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; }
+        public bool Debug { get; set; }
+    }
+}
+
+
